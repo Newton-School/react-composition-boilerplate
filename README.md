258d0266cc661ba7c6af787beb92eccd1329ce08
Akshat Sethi <akshat@nova-benefits.com>
2022-06-19 22:58:00 +0530

    done

---
+++ README.md
@@ -0,0 +1,9 @@
+# Component Composition
+
+In <code>src/components/App.js</code>
+We have two different components <code>Header</code> and
+<code>Footer</code>.
+
+Display them together ionside <code>App</code> component.
+
+
---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/App.css';
+
+const Header = () =>{
+  return (
+    <header>Hi, I am the header</header>
+  )
+}
+const Footer = () =>{
+  return (
+    <footer>This is the footer</footer>
+  )
+}
+const App = () => {
+
+  return (
+    <div id="main">
+
+    </div>
+  )
+}
+
+
+export default App;
